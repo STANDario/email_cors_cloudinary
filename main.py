04c77e562839496067d92d777c47a628718e36f4
Andrii Stoian <astoyan2002@gmail.com>
2023-12-18 21:11:19 +0200

    add auth and authorization

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from src.routes import contacts, auth
+
+
+app = FastAPI()
+
+
+app.include_router(auth.router, prefix="/api")
+app.include_router(contacts.router, prefix="/api")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hi! Thank you for visiting the site :)"}
