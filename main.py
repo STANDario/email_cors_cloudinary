e177a9e3ef3339bab5929936e62bac03f427f87c
Andrii Stoian <astoyan2002@gmail.com>
2023-12-27 14:04:18 +0200

    add CORS

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import redis.asyncio as redis
 from fastapi import FastAPI
 from fastapi_limiter import FastAPILimiter
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes import contacts, auth
 
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
