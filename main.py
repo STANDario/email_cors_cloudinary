6df74b23b2a0dfaabd0cc4f1132a18d8f3a62566
Andrii Stoian <astoyan2002@gmail.com>
2023-12-27 13:59:18 +0200

    add limit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import redis.asyncio as redis
 from fastapi import FastAPI
+from fastapi_limiter import FastAPILimiter
 
 from src.routes import contacts, auth
 
@@ -8,6 +10,12 @@ app = FastAPI()
 
 app.include_router(auth.router, prefix="/api")
 app.include_router(contacts.router, prefix="/api")
+
+
+@app.on_event("startup")
+async def startup():
+    r = await redis.Redis(host="localhost", port=6379, db=0, encoding="utf-8", decode_responses= True)
+    await FastAPILimiter.init(r)
 
 
 @app.get("/")
--- src/routes/contacts.py
+++ src/routes/contacts.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import APIRouter, HTTPException, Depends, status, Query, Path
+from fastapi_limiter.depends import RateLimiter
 from sqlalchemy.orm import Session
 
 from src.database.db import get_db
@@ -13,49 +14,56 @@ from src.database.models import User
 router = APIRouter(prefix="/contacts", tags=["contacts"])
 
 
-@router.get("/", response_model=List[ContactResponse])
+@router.get("/", response_model=List[ContactResponse],
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_contacts(limit: int = Query(default=10, le=50), skip: int = 0, db: Session = Depends(get_db),
                        current_user: User = Depends(auth_service.get_current_user)):
     contacts = await repository_contacts.get_contacts(limit, skip, current_user, db)
     return contacts
 
 
-@router.get("/search_by_email", response_model=ContactResponse)
+@router.get("/search_by_email", response_model=ContactResponse,
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_contact_by_email(contact_email: str, db: Session = Depends(get_db),
                                current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_email(contact_email, current_user, db)
     return contact
 
 
-@router.get("/search_by_name", response_model=List[ContactResponse])
+@router.get("/search_by_name", response_model=List[ContactResponse],
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_contact_by_name(contact_name: str, db: Session = Depends(get_db),
                               current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_name(contact_name, current_user, db)
     return contact
 
 
-@router.get("/search_by_surname", response_model=List[ContactResponse])
+@router.get("/search_by_surname", response_model=List[ContactResponse],
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_contact_by_surname(contact_surname: str, db: Session = Depends(get_db),
                                  current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_surname(contact_surname, current_user, db)
     return contact
 
 
-@router.get("/birthday", response_model=List[ContactResponse])
+@router.get("/birthday", response_model=List[ContactResponse],
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_birthday_contact(db: Session = Depends(get_db),
                                current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_birthday_contact(current_user, db)
     return contact
 
 
-@router.get("/{contact_id}", response_model=ContactResponse)
+@router.get("/{contact_id}", response_model=ContactResponse,
+            dependencies=[Depends(RateLimiter(times=2, seconds=5))])
 async def get_contact_by_id(contact_id: int = Path(ge=1), db: Session = Depends(get_db),
                             current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_id(contact_id, current_user, db)
     return contact
 
 
-@router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=ContactResponse, status_code=status.HTTP_201_CREATED,
+             dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 async def create_contact(body: ContactModel, db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.get_contact_by_phone(body.phone_number, current_user, db)
@@ -66,7 +74,8 @@ async def create_contact(body: ContactModel, db: Session = Depends(get_db),
     return contact
 
 
-@router.put("/{contact_id}", response_model=ContactResponse)
+@router.put("/{contact_id}", response_model=ContactResponse,
+            dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 async def update_contact(body: ContactModel, contact_id: int = Path(ge=1), db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.update_contact(body, db, current_user, contact_id)
@@ -75,7 +84,8 @@ async def update_contact(body: ContactModel, contact_id: int = Path(ge=1), db: S
     return contact
 
 
-@router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{contact_id}", status_code=status.HTTP_204_NO_CONTENT,
+               dependencies=[Depends(RateLimiter(times=1, seconds=5))])
 async def remove_contact(contact_id: int = Path(ge=1), db: Session = Depends(get_db),
                          current_user: User = Depends(auth_service.get_current_user)):
     contact = await repository_contacts.remove_contact(contact_id, current_user, db)
